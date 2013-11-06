754e65db135fc22bb865c7f5db6ddb0344dd4eee
Kalan MacRow <kalanwm@cs.ubc.ca>
2013-11-05 22:59:20 -0800

    add Server.py

---
+++ src/python/shellac/server/Server.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import argparse
+import socket, select
+
+class Server(object):
+
+    def __init__(self):
+        pass
+
+    def run():
+        pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
