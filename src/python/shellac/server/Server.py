3c47d24abdd247cdfab232d1640e47dfc30b6c0e
Kalan MacRow <kalanwm@cs.ubc.ca>
2013-11-22 01:14:40 -0800

    upstream integration

--- src/python/shellac/server/Server.py
+++ src/python/shellac/server/Server.py
@@ -3,26 +3,87 @@
 import os
 import sys
 import signal
+import random
 import argparse
 import logging
 import socket, select
 import pylibmc
 from http_parser.parser import HttpParser
 
-
 logger = logging.getLogger(__name__)
+
+class Stream(object):
+
+    def __init__(self, data = None, buffered = False):
+        self._buf = None
+        self._pos = 0
+        self._eos = False
+        self._ready = False
+        self._buffered = buffered
+
+        if data != None:
+            self.push(data)
+
+    def push(self, buf):
+        if self._buf is None:
+            self._buf = buf
+            self._ready = True
+        else:
+            self._buf += buf
+
+    def read(self):
+        if self._buffered:
+            return self._buf[self._pos:]
+        else:
+            return self._buf
+
+    def ack(self, bytes):
+        if bytes > 0:
+            if self._buffered:
+                self._pos += bytes
+            else:
+                self._buf = self._buf[bytes:]
+
+    def close(self):
+        self._eos = True
+
+    def buffer(self):
+        return self._buf
+
+    def complete(self):
+        if self._buffered:
+            return self._eos and self._pos == len(self._buf)
+        else:
+            return self._eos and len(self._buf) == 0
+
+    def ready(self):
+        return self._ready
 
 class Server(object):
 
     def __init__(self, port = 8080):
 
+        # fd => socket
         self._connections = {}
+
+        # fd => [HttpParser] (Queue)
         self._requests    = {}
+
+        # fd => [Response] (Queue)
         self._responses   = {}
 
-        self._cache_connections = {}
-        self._cache_requests    = {}
-        self._cache_responses   = {}
+        # (fd_up, req_num) => (fd_down, resp_num)
+        self._stream_map = {}
+
+        # names of upstream servers to balance traffic on
+        self._upstream_servers = []
+
+        # fd => socket
+        self._upstream_connections = {}
+
+        # fd => [Request] (Queue)
+        self._upstream_requests    = {}
+
 
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -33,65 +94,145 @@ class Server(object):
         self._epoll = select.epoll()
         self._epoll.register(self._socket.fileno(), select.EPOLLIN)
 
+    def _choose_upstream_fd(self):
+        return 0
+
     def _new_connection(self):
     
         connection, address = self._socket.accept()
         connection.setblocking(0)
-        fileno = connection.fileno()
+        fd = connection.fileno()
 
-        self._epoll.register(fileno, select.EPOLLIN)
-        self._connections[fileno] = connection
-        self._requests[fileno]    = HttpParser()
-        self._responses[fileno]   = b'HTTP/1.0 200 OK\r\nContent-type: text/plain\r\nContent-Length: 6\r\nConnection: close\r\n\r\nHello+'
-    
-    def _close_connection(self, fileno):
+        self._epoll.register(fd, select.EPOLLIN)
+        self._connections[fd] = connection
+        self._requests[fd]    = [HttpParser()]
+        self._responses[fd]   = []
+        
+        cork_socket(connection)
 
-        self._epoll.unregister(fileno)
-        self._connections[fileno].close()
-        del self._connections[fileno]
+    def _close_connection(self, fd):
 
-    def _send_response(self, fileno):
+        self._epoll.unregister(fd)
+        self._connections[fd].close()
+        del self._connections[fd]
+        del self._requests[fd]
+        del self._responses[fd]
 
-        sent = self._connections[fileno].send(self._responses[fileno])
-        self._responses[fileno] = self._responses[fileno][sent:]
-        if len(self._responses[fileno]) == 0:
-            self._epoll.modify(fileno, 0)
-            self._connections[fileno].shutdown(socket.SHUT_RDWR)
-            del self._requests[fileno]
-            del self._responses[fileno]
+    def _write_event(self, fd):
 
-    def _read_request(self, fileno):
+        if fd in self._connections:
 
-        data = self._connections[fileno].recv(4096)
-        self._requests[fileno].execute(data, len(data))
+            # write response to client
+            if len(self._responses[fd]) == 0:
+                return
 
-        if self._requests[fileno].is_headers_complete():
-            self._epoll.modify(fileno, select.EPOLLOUT)
+            (key, stream) = self._responses[fd][0]
+            if not stream.ready():
+                return
+
+            sent = self._connections[fd].send( stream.read() )
+            stream.ack( sent )
+
+            if stream.complete():
+                flush_socket(self._connections[fd])
+                # cache.put(key, stream.buffer())
+                self._responses[fd] = self._responses[fd][1:]
+                    
+        else:
+            # write request to upstream server
+            if len(self._upstream_requests[fd]) == 0:
+                return
+
+            stream = self._upstream_requests[fd][0]
+            if not stream.ready():
+                return
+
+            sent = self._upstream_connections[fd].send( stream.read() )
+            stream.ack( sent )
+
+            if stream.complete():
+                flush_socket(self._upstream_connections[fd])
+                self._upstream_requests[fd] = self._upstream_requests[fd][1:]
+
+
+    def _read_event(self, fileno):
+
+        if fd in self._connections:
+            # read request from client
+            data = self._connections[fd].recv(4096)
+
+            request = self._requests[fd][-1]
+            request.execute(data, len(data))
+
+            if request.is_message_complete():
+                # create a response
+                #key = request.get_url()
+                #if cache.has(key):
+                #    self._responses[fd].append( ( key, Stream(cache.get(key)) ) )
+                #else:
+                self._responses[fd].append( ( key, Stream(data=None, buffered=True) ) )
                 
+                up_fd = self._choose_upstream_fd()
+                self._upstream_requests[up_fd].append( Stream( http_request_str(request) ) )
+                
+                self._stream_map[up_fd].append(fd)
+            
+            
+        else:
+            # read response from upstream server
+            data = self._upstream_connections[fd].recv(4096)
+            
+            down_fd = self._stream_map[fd][0]
+            (_, response) = self._responses[down_fd][0]
+            response.push(data)
+
+            if len(data) == 0:
+                response.close()
+                self._stream_map[fd] = self._stream_map[fd][1:]
 
     def run(self):
         
         try:
             while True:
                 events = self._epoll.poll(1)
-                for fileno, event in events:
+                for fd, event in events:
                     
-                    if fileno == self._socket.fileno():
+                    if fd == self._socket.fileno():
                         self._new_connection()
 
                     elif event & select.EPOLLIN:
-                        self._read_request(fileno)
+                        self._read_event(fd)
 
                     elif event & select.EPOLLOUT:
-                        self._send_response(fileno)
+                        self._write_event(fd)
 
                     elif event & select.EPOLLHUP:
-                        self._close_connection(fileno)
+                        self._close_connection(fd)
                     
         finally:
             self._epoll.unregister(self._socket.fileno())
             self._epoll.close()
             self._socket.close()
+
+def cork_socket( sock ):
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CORK, 1)
+
+def flush_socket( sock ):
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_CORK, 0)
+
+def http_request_str( request ):
+    req = ''
+    req = '%s %s HTTP/1.1\r\n' % (request.get_method(), request.get_url())
+    
+    headers = request.get_headers()
+    headers['Connection'] = 'keep-alive'
+
+    for header in headers:
+        req += '%s: %s\r\n' % (header, headers[header])
+
+    req += '\r\n'
+    req += request.recv_body()
+    return req
 
 def main():
     parser = argparse.ArgumentParser(description='Shellac Accelerator')
