644aec915434c012010042832a6fe42415be5143
Kalan MacRow <kalanwm@cs.ubc.ca>
2013-11-27 09:18:26 -0800

    factor Stream out of the main server

--- src/python/shellac/server/Server.py
+++ src/python/shellac/server/Server.py
@@ -9,59 +9,10 @@ import logging
 import socket, select
 import pylibmc
 from http_parser.parser import HttpParser
+from Stream import Stream
 
 logger = logging.getLogger(__name__)
 
-class Stream(object):
-
-    def __init__(self, data = None, buffered = False):
-        self._buf = None
-        self._pos = 0
-        self._eos = False
-        self._ready = False
-        self._buffered = buffered
-
-        if data != None:
-            self.push(data)
-
-    def push(self, buf):
-        if self._buf is None:
-            self._buf = buf
-            self._ready = True
-        else:
-            self._buf += buf
-
-    def read(self):   
-        if not self._ready:
-            return None
-        if self._buffered:
-            return self._buf[self._pos:]
-        else:
-            return self._buf
-
-    def ack(self, bytes):
-        if not self._ready:
-            return
-        if bytes > 0:
-            if self._buffered:
-                self._pos += bytes
-            else:
-                self._buf = self._buf[bytes:]
-
-    def close(self):
-        self._eos = True
-
-    def buffer(self):
-        return self._buf
-
-    def complete(self):
-        if self._buffered:
-            return self._eos and self._pos == len(self._buf)
-        else:
-            return self._eos and len(self._buf) == 0
-
-    def ready(self):
-        return self._ready
 
 class Server(object):
 
