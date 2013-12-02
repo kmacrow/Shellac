02a4b374fd7f15788a74b18d65ec1c6e718e6edd
Kalan MacRow <kalanwm@cs.ubc.ca>
2013-12-01 22:32:18 -0800

    unit tests for HttpParser

--- src/python/shellac/server/HttpParser.py
+++ src/python/shellac/server/HttpParser.py
@@ -7,16 +7,17 @@ import cStringIO
 class HttpParser(object):
 
     def __init__(self):
-    	self._method = None
-    	self._version = None
-    	self._url = None
+        self._method = None
+        self._version = None
+        self._url = None
         self._status = None
         self._headers = {}
         self._body = cStringIO.StringIO()
         self._buf = ''
         self._is_request = True
-        self._content_len = 0
+        self._content_len = None
         self._chunked = False
+        self._last_chunk = False
 
         self._on_first_line = True
         self._on_headers = False
@@ -26,52 +27,51 @@ class HttpParser(object):
         self._message_complete = False
 
     def method(self):
-    	return self._method
+        return self._method
 
     def url(self):
-    	return self._url
+        return self._url
 
     def status(self):
         return self._status
 
     def version(self):
-    	return self._version
+        return self._version
 
     def headers(self):
-    	return self._headers
+        return self._headers
 
     def body(self):
-    	return self._body
+        return self._body
 
     def is_request(self):
-    	return self._is_request
+        return self._is_request
 
     def is_response(self):
-    	return not self._is_request
+        return not self._is_request
 
     def headers_complete(self):
-    	return self._headers_complete
+        return self._headers_complete
 
     def message_complete(self):
-    	return self._message_complete
+        return self._message_complete
 
-    def execute(self, data, length):
-    	""" parse data, return number of bytes consumed """
+    def parse(self, data, length):
+        """ Parse data, return number of bytes consumed. """
         
-    	if length == 0:
-    		self._message_complete = True
-    		return
+        if length == 0:
+            return
 
-    	nb_parsed = 0
+        nb_parsed = 0
 
         while True:
 
-        	if self._on_first_line:
+            if self._on_first_line:
 
-        		buf_len = len(self._buf)
-        		self._buf += data
+                buf_len = len(self._buf)
+                self._buf += data
 
-        		idx = self._buf.find('\r\n')
+                idx = self._buf.find('\r\n')
                 if idx < 0:
                     return length
                 
@@ -87,110 +87,144 @@ class HttpParser(object):
                 
                 return nb_parsed
                     
-        	elif self._on_headers:
+            elif self._on_headers:
 
-        		buf_len = len(self._buf)
-        		self._buf += data
+                buf_len = len(self._buf)
+                self._buf += data
 
-        		idx = self._buf.find('\r\n\r\n')
-        		if idx < 0:
-        			return length
+                idx = self._buf.find('\r\n\r\n')
+                if idx < 0:
+                    return length
 
-        		self._buf = self._buf[:idx + 4]
+                self._buf = self._buf[:idx + 4]
 
-        		self._parse_headers()
+                self._parse_headers()
 
-        		nb_parsed = len(self._buf) - buf_len
+                nb_parsed = len(self._buf) - buf_len
 
-        		self._buf = ''
-        		self._headers_complete = True
-        		self._on_headers = False
-        		self._on_body = True
+                self._buf = ''
+                self._headers_complete = True
+                self._on_headers = False
+                self._on_body = True
 
-        		if self._method in ['GET','HEAD']:
-        			self._message_complete = True
+                if self._method in ['GET','HEAD']:
+                    self._message_complete = True
 
-        		self._chunked = self._headers.get('Transfer-Encoding', 'none') == 'chunked'
-        		
-        		if not self._chunked:
-        			self._content_len = int(self._headers.get('Content-Length', 0))
-        			if self._content_len == 0:
-        				self._on_body = False
-        				self._message_complete = True
+                self._chunked = self._headers.get('transfer-encoding', 'none') == 'chunked'
+                
+                if not self._chunked:
+                    self._content_len = int(self._headers.get('content-length', 0))
+                    if self._content_len == 0:
+                        self._on_body = False
+                        self._message_complete = True
 
-        		return nb_parsed
+                return nb_parsed
 
-        	elif self._on_body:
+            elif self._on_body:
 
-        		if not self._chunked:
-        			
-        			if length <= self._content_len:
-        				self._buf = data
-        				self._parse_body()
-        				self._buf = ''
-        				self._content_len -= length
-        				return length
-        			else:
-        				self._buf = data[:self._content_len]
-        				self._parse_body()
-        				self._buf = ''
+                if not self._chunked:
 
-        				nb_parsed = self._content_len
+                    if length < self._content_len:
+                        self._buf = data
+                        self._parse_body()
+                        self._buf = ''
+                        self._content_len -= length
+                        return length
+                    else:
+                        self._buf = data[:self._content_len]
+                        self._parse_body()
+                        self._buf = ''
 
-        				self._content_len = 0
-        				self._on_body = False
-        				self._message_complete = True
-        				return nb_parsed
+                        nb_parsed = self._content_len
 
-        		else:
+                        self._content_len = 0
+                        self._on_body = False
+                        self._message_complete = True
+                        return nb_parsed
 
-        			# read chunked message body
-        			return self._parse_chunked(data, length)
+                else:
+
+                    # read chunked message body
+                    return self._parse_chunked(data, length)
 
 
-        	else:
-        		return 0
+            else:
+                return 0
+
+    def _parse_chunk_size(self, line):
+        """ Parse chunk header size """
+
+        size = line.strip()
+        if ';' in size:
+            return int(size.split(';')[0], 16) 
+        else:
+            return int(size, 16)
 
     def _parse_chunked(self, data, length):
+        """ Parse a chunked response """
 
-    	buf_len = len(self._buf)
-		self._buf += data
-		nb_parsed = length
+        buf_len = len(self._buf)
+        self._buf += data
+        
+        nb_parsed = 0
 
-    	while True:	
+        if self._content_len is None:
 
-			idx = self._buf.find('\r\n')
-			if idx < 0:
-				return nb_parsed
+            match = re.match(r'(\r\n)?[a-z0-9]+\r\n', self._buf, flags=re.IGNORECASE)
 
-			line = self._buf[:idx]
-			self._content_len = self._parse_chunk_size(line)
-			
-			nb_parsed = idx + 2 - buf_len
-			self._buf = self._buf[idx + 2:]
+            if match is None:
+                return length
 
-			# read chunk data
-			left = len(self._buf)
+            idx = len(match.group())
 
-			if left <= self._content_len:
-				self._parse_body()
-				self._content_len -= left
-				self._buf = ''
-				nb_parsed += left
-				return nb_parsed
-			else:
-				next = self._buf[self._content_len:]
-				self._buf = self._buf[:self._content_len]
-				self._parse_body()
-				nb_parsed += self._content_len
-				self._content_len = 0
-				self._buf = next
+            line = self._buf[:idx]
+            self._content_len = self._parse_chunk_size(line)
+            
+            nb_parsed = idx - buf_len
+            self._buf = self._buf[idx:]
+
+            left = len(self._buf)
+
+            if self._content_len == 0:
+                if self._buf.startswith('\r\n'):
+                    nb_parsed += 2
+                    self._on_body = False
+                    self._message_complete = True
+
+                self._buf = ''
+                return nb_parsed
+        
+        elif self._content_len == 0:
+            if self._buf.startswith('\r\n'):
+                nb_parsed = 2 - buf_len
+                self._buf = ''
+                self._on_body = False
+                self._message_complete = True
+                return nb_parsed
+            else:
+                return length
+        else:
+            left = length
+
+        if left < self._content_len:
+            self._parse_body()
+            self._content_len -= left
+            self._buf = ''
+            nb_parsed += left
+            return nb_parsed
+        else:
+            self._buf = self._buf[:self._content_len]
+            self._parse_body()
+            self._buf = ''
+            nb_parsed += self._content_len
+            self._content_len = None
+            return nb_parsed
 
 
     def _parse_first_line(self):
-    	""" Parse a request/response line """
+        """ Parse a request/response line """
 
-        (a, b, c) = self._buf.rstrip().split(' ')
+        (a, b, c) = self._buf.rstrip().split(' ', 2)
         if a.startswith('HT'):
             # response...
             self._is_request = False
@@ -206,8 +240,8 @@ class HttpParser(object):
             self._url = b
 
     def _parse_headers(self):
-    	""" Parse raw headers into a dict of scalars/lists """
-        
+        """ Parse raw headers into a dict of scalars/lists """
+
         headers = self._buf.strip().split('\r\n')
         for header in headers:
             (key, value) = header.split(': ')
@@ -223,7 +257,216 @@ class HttpParser(object):
 
 
     def _parse_body(self):
-    	pos = self._body.tell()
-		self._body.seek(0, os.SEEK_END)
-		self._body.write(self._buf)
-		self._body.seek(pos)
+        pos = self._body.tell()
+        self._body.seek(0, os.SEEK_END)
+        self._body.write(self._buf)
+        self._body.seek(pos)
+
+
+def test():
+
+    def dump_parser(p):
+        print '######'
+        if p.is_request():
+            print 'Method: %s' % p.method()
+            print 'Url: %s' % p.url()
+
+        print 'Version: %f' % p.version()
+        
+        if p.is_response():
+            print 'Status: %d' % p.status()
+
+        headers = p.headers()
+        for header in headers:
+            print '%s: %s' % (header, str(headers[header]))
+        print '------'
+        print p.body().getvalue()
+
+    print 'Testing...'
+
+    req = 'GET /get-request.html HTTP/1.1\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= '\r\n'
+
+    p = HttpParser()
+    while not p.message_complete():
+        c = p.parse(req, len(req))
+        req = req[c:]
+
+    dump_parser(p)
+
+    assert p.method() == 'GET'
+    assert p.url() == '/get-request.html'
+    assert p.version() == 1.1
+    assert p.headers()['user-agent'] == 'Safari'
+
+    req = 'POST /post-request.html HTTP/1.1\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Content-Length: 10\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= '\r\n'
+    req+= 'XXXXXXXXXX'
+
+    p = HttpParser()
+    while not p.message_complete():
+        c = p.parse(req, len(req))
+        req = req[c:]
+
+    assert p.method() == 'POST'
+    assert p.url() == '/post-request.html'
+    assert p.version() == 1.1
+    assert p.headers()['user-agent'] == 'Safari'
+
+    dump_parser(p)
+
+    # a request stream...
+    req = 'GET /stream1.html HTTP/1.1\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= '\r\n'
+    req+= 'POST /stream2.html HTTP/1.1\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Content-Length: 10\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= '\r\n'
+    req+= 'XXXXXXXXXX'
+    req+= 'POST /stream3.html HTTP/1.1\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Content-Length: 20\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= '\r\n'
+    req+= 'XXXXXXXXXXXXXXXXXXXX'
+
+    p = []
+    while len(req):
+        h = HttpParser()
+        while not h.message_complete():
+            c = h.parse(req, len(req))
+            req = req[c:]
+        p.append(h)
+
+    assert len(p) == 3
+
+    for h in p:
+        print
+        assert h.message_complete() == True
+        dump_parser(h)    
+
+    # a response stream...
+    req = 'HTTP/1.1 200 OK\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= 'Content-Length: 10\r\n'
+    req+= '\r\n'
+    req+= 'XXXXXXXXXX'
+    req+= 'HTTP/1.1 200 OK\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Content-Length: 20\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= '\r\n'
+    req+= 'XXXXXXXXXXXXXXXXXXXX'
+    req+= 'HTTP/1.1 200 OK\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Content-Length: 30\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= '\r\n'
+    req+= 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    req+= 'HTTP/1.1 200 OK\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Content-Length: 0\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= '\r\n'
+    req+= 'HTTP/1.1 302 Not Modified\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= '\r\n'
+    
+
+    p = []
+    while len(req):
+        h = HttpParser()
+        while not h.message_complete():
+            c = h.parse(req, len(req))
+            req = req[c:]
+        p.append(h)
+
+    assert len(p) == 5
+
+    for h in p:
+        print
+        assert h.message_complete() == True
+        dump_parser(h)
+
+    p = HttpParser()
+
+    # parse in pieces...
+    req = 'HTTP/1.1 200 OK\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+
+    while len(req) != 0:
+        c = p.parse(req, len(req))
+        req = req[c:]
+
+    req = 'Content-Length: 10\r\n'
+    req+= '\r\n'
+    req+= 'XXXXX'
+
+    while len(req) != 0:
+        c = p.parse(req, len(req))
+        req = req[c:]
+
+    assert p.headers_complete() == True
+    assert p.body().read() == 'XXXXX'
+
+    req = 'AAAAA'
+    
+    while len(req) != 0:
+        c = p.parse(req, len(req))
+        req = req[c:]
+
+    assert p.body().read() == 'AAAAA'
+
+    assert p.message_complete() == True
+    dump_parser(p)
+
+    # chunked message body...
+    req+= 'HTTP/1.1 200 OK\r\n'
+    req+= 'User-Agent: Safari\r\n'
+    req+= 'User-Agent: Mac OS 10.8\r\n'
+    req+= 'Transfer-Encoding: chunked\r\n'
+    req+= 'Date: Jul 25, 2013 5:14:11 GMT\r\n'
+    req+= '\r\n'
+    req+= 'A\r\n'
+    req+= 'AAAAAAAAAA'
+    req+= '\r\n'
+    req+= '8\r\n'
+    req+= 'BBBBBBBB'
+    req+= '\r\n'
+    req+= '6\r\n'
+    req+= 'CCCCCC'
+    req+= '\r\n'
+    req+= '0\r\n'
+    req+= '\r\n'
+
+    p = HttpParser()
+    while not p.message_complete():
+        c = p.parse(req, len(req))
+        req = req[c:]
+
+    assert p.body().read() == 'AAAAAAAAAABBBBBBBBCCCCCC'
+
+    print
+    dump_parser(p)
+
+    # chunked message stream...
+
+    # chunked message in pieces...
+
+    print
+    print 'Done.'
+    print 
+
+if __name__ == '__main__':
+    test()
